c054e737937cf842a91764969302681a97e6d303
22alune <209618604+22alune@users.noreply.github.com>
2025-05-09 13:08:20 +0200

    The possibility to hide debug menu for backstage.

--- PiRPG/src/system/ui/debug.gd
+++ PiRPG/src/system/ui/debug.gd
@@ -4,3 +4,12 @@ extends Control
 func _enter_tree() -> void:
 	if not OS.has_feature("debug"):
 		queue_free()
+
+
+func _unhandled_key_input(_event: InputEvent) -> void:
+	if Input.is_key_label_pressed(KEY_U):
+		visible = !visible
+
+
+func _on_hide_debug_button_pressed() -> void:
+	hide()
