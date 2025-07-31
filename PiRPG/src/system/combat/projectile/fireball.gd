50593156f0facc49a52f436ebe575f876b991f5a
22alune <209618604+22alune@users.noreply.github.com>
2025-07-31 23:44:25 +0200

    Update fireball.gd
    Replace blocking instruction by a simple signal.

--- PiRPG/src/system/combat/projectile/fireball.gd
+++ PiRPG/src/system/combat/projectile/fireball.gd
@@ -3,17 +3,20 @@ extends Area2D
 # General variables
 @export var projectile_speed: int = 240
 @export var lifetime: float = 1.5
+
 var direction := Vector2.ZERO
 
-func _ready() -> void:
-	await get_tree().create_timer(lifetime).timeout # Start a timer of 'lifetime'
-	queue_free()
 
-# This function updates every second (Delta is the time passed since the last frame)
+func _ready() -> void:
+	# Delete the fire ball when lasting too long.
+	get_tree().create_timer(lifetime).timeout.connect(queue_free())
+
+
 func _process(delta: float) -> void:
 	position += direction * projectile_speed * delta # Move the projectile in the direction is facing
 	if position.x < -100 or position.x > 2000:
 		queue_free() # If it gets out of the screen, destroy it.
+
 
 func _on_area_entered(area: Area2D) -> void:
 	# Here we'll manage if the fireball hit and enemy or another object!
