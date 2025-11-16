1a9ac7b99548d1903e111b774662755e15b764c5
Shayan-Bidelogne <shayanbidelogne07@gmail.com>
2025-11-16 11:22:12 +0100

    e

--- commands/twitter_feed_listener.py
+++ commands/twitter_feed_listener.py
@@ -4,26 +4,16 @@ from discord.ext import commands, tasks
 import tweepy
 
 # ================== CONFIG depuis l'environnement ==================
-TWITTER_API_KEY = os.environ.get("TWITTER_API_KEY")
-TWITTER_API_SECRET = os.environ.get("TWITTER_API_SECRET")
-TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
-TWITTER_ACCESS_TOKEN_SECRET = os.environ.get("TWITTER_ACCESS_TOKEN_SECRET")
+BEARER_TOKEN = os.environ.get("TWITTER_BEARER_TOKEN")
 TWITTER_USERNAME = "pi.rpg"
-DISCORD_CHANNEL_ID = int(os.environ.get("DISCORD_CHANNEL_ID", "1439549538556973106"))  # converti en int
-CHECK_INTERVAL_MINUTES = int(os.environ.get("CHECK_INTERVAL_MINUTES", "1"))  
+DISCORD_CHANNEL_ID = int(os.environ.get("DISCORD_CHANNEL_ID", "1439549538556973106"))
+CHECK_INTERVAL_MINUTES = int(os.environ.get("CHECK_INTERVAL_MINUTES", "1"))
 # ==================================================================
 
 class TwitterFeedListener(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
-
-        # Authentification OAuth1
-        auth = tweepy.OAuth1UserHandler(
-            TWITTER_API_KEY, TWITTER_API_SECRET,
-            TWITTER_ACCESS_TOKEN, TWITTER_ACCESS_TOKEN_SECRET
-        )
-        self.client = tweepy.API(auth)
-
+        self.client = tweepy.Client(bearer_token=BEARER_TOKEN)
         self.posted_tweet_ids = set()  # Pour éviter les doublons
         self.check_tweets.start()
 
@@ -33,20 +23,23 @@ class TwitterFeedListener(commands.Cog):
     @tasks.loop(minutes=CHECK_INTERVAL_MINUTES)
     async def check_tweets(self):
         try:
-            tweets = self.client.user_timeline(
-                screen_name=TWITTER_USERNAME,
-                count=5,
-                tweet_mode="extended"
-            )
+            # Récupère l'ID numérique du compte
+            user = self.client.get_user(username=TWITTER_USERNAME)
+            if not user or not user.data:
+                return
+            user_id = user.data.id
 
-            if not tweets:
+            # Récupère les 5 derniers tweets
+            tweets = self.client.get_users_tweets(id=user_id, max_results=5, tweet_fields=["created_at", "entities"])
+            if not tweets.data:
                 return
 
             channel = self.bot.get_channel(DISCORD_CHANNEL_ID)
-            for tweet in reversed(tweets):  # du plus ancien au plus récent
+            for tweet in reversed(tweets.data):  # du plus ancien au plus récent
                 if tweet.id not in self.posted_tweet_ids:
+                    # Création de l'embed
                     embed = discord.Embed(
-                        description=tweet.full_text,
+                        description=tweet.text,
                         color=discord.Color.blue(),
                         timestamp=tweet.created_at
                     )
@@ -55,9 +48,12 @@ class TwitterFeedListener(commands.Cog):
                         url=f"https://twitter.com/{TWITTER_USERNAME}/status/{tweet.id}"
                     )
 
-                    media = tweet.entities.get("media", [])
-                    if media:
-                        embed.set_image(url=media[0]["media_url_https"])
+                    # Si le tweet contient des liens médias
+                    media_urls = []
+                    if tweet.entities and "urls" in tweet.entities:
+                        media_urls = [url["expanded_url"] for url in tweet.entities["urls"]]
+                    if media_urls:
+                        embed.add_field(name="Lien", value="\n".join(media_urls), inline=False)
 
                     await channel.send(embed=embed)
                     self.posted_tweet_ids.add(tweet.id)
